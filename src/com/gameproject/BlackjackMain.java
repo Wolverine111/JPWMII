4edf4a649706f32331b5712f5fd695b0bc7df403
Wolverine111 <morfeusz454@gmail.com>
2015-12-01 19:35:31 +0100

    Połączenie menu i gry

--- src/com/gameproject/BlackjackMain.java
+++ src/com/gameproject/BlackjackMain.java
@@ -2,6 +2,8 @@ package com.gameproject;
 
 import com.gameproject.Deck;
 import com.gameproject.Hand;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -11,6 +13,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.Glow;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
@@ -20,9 +26,15 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+import javafx.util.Duration;
 
 public class BlackjackMain extends Application {
+
+    // from game menu
+    private GameMenu gameMenu;
+    private int windowWidth = 800;
+    private int windowHeight = 600;
+    Pane root = new Pane();
 
     private Deck deck = new Deck();
     private Hand dealer, player;
@@ -38,11 +50,11 @@ public class BlackjackMain extends Application {
         player = new Hand(playerCards.getChildren());
 
         Pane root = new Pane();
-        root.setPrefSize(800, 600);
+        root.setPrefSize(windowWidth, windowHeight);
 
         Region background = new Region();
-        background.setPrefSize(800, 600);
-        background.setStyle("-fx-background-color: rgba(0, 0, 0, 1)");
+        background.setPrefSize(windowWidth, windowHeight);
+        background.setStyle("-fx-background-image: url('res/images/casino.jpg')");
 
         HBox rootLayout = new HBox(5);
         rootLayout.setPadding(new Insets(5, 5, 5, 5));
@@ -65,15 +77,13 @@ public class BlackjackMain extends Application {
         leftVBox.getChildren().addAll(dealerScore, dealerCards, message, playerCards, playerScore);
 
         // RIGHT
-
         VBox rightVBox = new VBox(20);
         rightVBox.setAlignment(Pos.CENTER);
 
-        final TextField bet = new TextField("BET");
-        bet.setDisable(true);
-        bet.setMaxWidth(50);
-        Text money = new Text("MONEY");
-
+        //final TextField bet = new TextField("BET");
+        //bet.setDisable(true);
+        //bet.setMaxWidth(50);
+        //Text money = new Text("MONEY");
         Button btnPlay = new Button("PLAY");
         Button btnHit = new Button("HIT");
         Button btnStand = new Button("STAND");
@@ -81,15 +91,14 @@ public class BlackjackMain extends Application {
         HBox buttonsHBox = new HBox(15, btnHit, btnStand);
         buttonsHBox.setAlignment(Pos.CENTER);
 
-        rightVBox.getChildren().addAll(bet, btnPlay, money, buttonsHBox);
+        //rightVBox.getChildren().addAll(bet, btnPlay, money, buttonsHBox);
+        rightVBox.getChildren().addAll(btnPlay, buttonsHBox);
 
         // ADD BOTH STACKS TO ROOT LAYOUT
-
         rootLayout.getChildren().addAll(new StackPane(leftBG, leftVBox), new StackPane(rightBG, rightVBox));
         root.getChildren().addAll(background, rootLayout);
 
         // BIND PROPERTIES
-
         btnPlay.disableProperty().bind(playable);
         btnHit.disableProperty().bind(playable.not());
         btnStand.disableProperty().bind(playable.not());
@@ -110,7 +119,6 @@ public class BlackjackMain extends Application {
         });
 
         // INIT BUTTONS
-
         btnPlay.setOnAction(event -> {
             startNewGame();
         });
@@ -156,17 +164,243 @@ public class BlackjackMain extends Application {
         if (dealerValue == 21 || playerValue > 21 || dealerValue == playerValue
                 || (dealerValue < 21 && dealerValue > playerValue)) {
             winner = "DEALER";
-        }
-        else if (playerValue == 21 || dealerValue > 21 || playerValue > dealerValue) {
+        } else if (playerValue == 21 || dealerValue > 21 || playerValue > dealerValue) {
             winner = "PLAYER";
         }
 
         message.setText(winner + " WON");
     }
 
+    //from game menu
+    private class GameMenu extends Parent {
+
+        public GameMenu() {
+
+            VBox menu0 = new VBox(10);
+            VBox menu1 = new VBox(10);
+            VBox menu2 = new VBox(10);
+
+            menu0.setTranslateX(100);
+            menu0.setTranslateY(200);
+
+            menu1.setTranslateX(100);
+            menu1.setTranslateY(200);
+
+            menu2.setTranslateX(100);
+            menu2.setTranslateY(200);
+
+            final int offset = 400;
+
+            menu1.setTranslateX(offset);
+            menu2.setTranslateX(offset);
+
+            BlackjackMain.MenuButton btnNewGame = new BlackjackMain.MenuButton("NEW GAME");
+            btnNewGame.setOnMouseClicked(event -> {
+                FadeTransition ft = new FadeTransition(Duration.seconds(0.5), this);
+                ft.setFromValue(1);
+                ft.setToValue(0);
+                ft.setOnFinished(evt -> setVisible(false));
+                ft.play();
+
+            });
+
+//			BlackjackMain.MenuButton btnResume = new BlackjackMain.MenuButton("RESUME");
+//			btnResume.setOnMouseClicked(event -> {
+//				FadeTransition ft = new FadeTransition(Duration.seconds(0.5), this);
+//				ft.setFromValue(1);
+//				ft.setToValue(0);
+//				ft.setOnFinished(evt -> setVisible(false));
+//				ft.play();
+//			});
+            BlackjackMain.MenuButton btnOptions = new BlackjackMain.MenuButton("OPTIONS");
+            btnOptions.setOnMouseClicked(event -> {
+                getChildren().add(menu1);
+
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), menu0);
+                tt.setToX(menu0.getTranslateX() - offset);
+
+                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), menu1);
+                tt1.setToX(menu0.getTranslateX());
+
+                tt.play();
+                tt1.play();
+
+                tt.setOnFinished(evt -> {
+                    getChildren().remove(menu0);
+                });
+
+            });
+
+            BlackjackMain.MenuButton btnExit = new BlackjackMain.MenuButton("EXIT");
+            btnExit.setOnMouseClicked(event -> {
+                System.exit(0);
+            });
+
+            BlackjackMain.MenuButton btnBack = new BlackjackMain.MenuButton("BACK");
+            btnBack.setOnMouseClicked(event -> {
+                getChildren().add(menu0);
+
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), menu1);
+                tt.setToX(menu1.getTranslateX() + offset);
+
+                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), menu0);
+                tt1.setToX(menu1.getTranslateX());
+
+                tt.play();
+                tt1.play();
+
+                tt.setOnFinished(evt -> {
+                    getChildren().remove(menu1);
+                });
+            });
+
+            BlackjackMain.MenuButton btnSound = new BlackjackMain.MenuButton("SOUND");
+            BlackjackMain.MenuButton btnVideo = new BlackjackMain.MenuButton("VIDEO");
+            btnVideo.setOnMouseClicked(event -> {
+                getChildren().add(menu2);
+
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), menu1);
+                tt.setToX(menu1.getTranslateX() - offset);
+
+                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), menu2);
+                tt1.setToX(menu1.getTranslateX());
+
+                tt.play();
+                tt1.play();
+
+                tt.setOnFinished(evt -> {
+                    getChildren().remove(menu1);
+                });
+
+            });
+
+            BlackjackMain.MenuButton btnLowRes = new BlackjackMain.MenuButton("640x480");
+            btnLowRes.setOnMouseClicked(event -> {
+                windowWidth = 640;
+                windowHeight = 480;
+                /*stage.setHeight(windowHeight);
+                 stage.setWidth(windowWidth);
+                 */
+
+            });
+
+            BlackjackMain.MenuButton btnHighRes = new BlackjackMain.MenuButton("800x600");
+            btnHighRes.setOnMouseClicked(event -> {
+                windowWidth = 800;
+                windowHeight = 600;
+            });
+            BlackjackMain.MenuButton btnBack2 = new BlackjackMain.MenuButton("BACK");
+            btnBack2.setOnMouseClicked(event -> {
+                getChildren().add(menu1);
+
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), menu2);
+                tt.setToX(menu2.getTranslateX() + offset);
+
+                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), menu1);
+                tt1.setToX(menu2.getTranslateX());
+
+                tt.play();
+                tt1.play();
+
+                tt.setOnFinished(evt -> {
+                    getChildren().remove(menu2);
+                });
+            });
+
+            menu0.getChildren().addAll(btnNewGame, btnOptions, btnExit);
+            menu1.getChildren().addAll(btnBack, btnSound, btnVideo);
+            menu2.getChildren().addAll(btnBack2, btnLowRes, btnHighRes);
+
+            Rectangle bg = new Rectangle(windowWidth, windowHeight);
+            bg.setFill(Color.GREY);
+            bg.setOpacity(0.8);
+
+            getChildren().addAll(bg, menu0);
+
+        }
+
+    }
+
+    private static class MenuButton extends StackPane {
+
+        private Text text;
+
+        public MenuButton(String name) {
+            text = new Text(name);
+            text.setFont(text.getFont().font(20));
+            text.setFill(Color.WHITE);
+
+            Rectangle bg = new Rectangle(250, 30);
+            bg.setOpacity(0.6);
+            bg.setFill(Color.BLACK);
+            bg.setEffect(new GaussianBlur(3.5));
+
+            setAlignment(Pos.CENTER_LEFT);
+            setRotate(-0.5);
+            getChildren().addAll(bg, text);
+
+            setOnMouseEntered(event -> {
+                bg.setTranslateX(10);
+                text.setTranslateX(10);
+                bg.setFill(Color.WHITE);
+                text.setFill(Color.BLACK);
+            });
+
+            setOnMouseExited(event -> {
+                bg.setTranslateX(0);
+                text.setTranslateX(0);
+                bg.setFill(Color.BLACK);
+                text.setFill(Color.WHITE);
+            });
+
+            DropShadow drop = new DropShadow(50, Color.WHITE);
+            drop.setInput(new Glow());
+
+            setOnMousePressed(event -> setEffect(drop));
+            setOnMouseReleased(event -> setEffect(null));
+
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(createContent()));
+
+        root = new Pane((Pane) createContent());
+        //from game menu
+        root.setPrefSize(windowWidth, windowHeight);
+
+        gameMenu = new BlackjackMain.GameMenu();
+        gameMenu.setVisible(true);
+
+        root.getChildren().addAll(gameMenu);
+
+                //root.setStyle("-fx-background-image: url('res/images/casino.jpg')");
+        Scene scene = new Scene(root);
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ESCAPE) {
+                if (!gameMenu.isVisible()) {
+                    FadeTransition ft = new FadeTransition(Duration.seconds(0.5), gameMenu);
+                    ft.setFromValue(0);
+                    ft.setToValue(1);
+
+                    gameMenu.setVisible(true);
+                    ft.play();
+                } else {
+                    FadeTransition ft = new FadeTransition(Duration.seconds(0.5), gameMenu);
+                    ft.setFromValue(1);
+                    ft.setToValue(0);
+                    ft.setOnFinished(evt -> gameMenu.setVisible(false));
+                    ft.play();
+
+                }
+            }
+        });
+
+                //resize(primaryStage);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        //primaryStage.setScene(new Scene(createContent()));
         primaryStage.setWidth(800);
         primaryStage.setHeight(600);
         primaryStage.setResizable(false);
